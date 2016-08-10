68194040241410c54e95df26e33dd0244c492ce4
mgoofyy <mgoofyy@gmail.com>
2016-08-10 22:36:54 +0800

    add tableview section

---
+++ HRContactDataUtil.h
@@ -0,0 +1,16 @@
+//
+//  HRContactDataUtil.h
+//  HRManger
+//
+//  Created by goofygao on 8/6/16.
+//  Copyright © 2016 goofyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HRContactDataUtil : NSObject
+
++ (NSMutableArray *) friendListDataBy:(NSMutableArray *)array;
++ (NSMutableArray *) friendListSectionBy:(NSMutableArray *)array;
+
+@end
---
+++ HRContactDataUtil.m
@@ -0,0 +1,85 @@
+//
+//  HRContactDataUtil.m
+//  HRManger
+//
+//  Created by goofygao on 8/6/16.
+//  Copyright © 2016 goofyy. All rights reserved.
+//
+
+#import "HRContactDataUtil.h"
+#import "HRContactModel.h"
+
+@implementation HRContactDataUtil
+
++ (NSMutableArray *) friendListDataBy:(NSMutableArray *)array{
+    NSMutableArray *ans = [[NSMutableArray alloc] init];
+    
+    NSArray *serializeArray = [(NSArray *)array sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {//排序
+        int i;
+        NSString *strA = ((HRContactModel *)obj1).pinyin;
+        NSString *strB = ((HRContactModel *)obj2).pinyin;
+        for (i = 0; i < strA.length && i < strB.length; i ++) {
+            char a = [strA characterAtIndex:i];
+            char b = [strB characterAtIndex:i];
+            if (a > b) {
+                return (NSComparisonResult)NSOrderedDescending;//上升
+            }
+            else if (a < b) {
+                return (NSComparisonResult)NSOrderedAscending;//下降
+            }
+        }
+        
+        if (strA.length > strB.length) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }else if (strA.length < strB.length){
+            return (NSComparisonResult)NSOrderedAscending;
+        }else{
+            return (NSComparisonResult)NSOrderedSame;
+        }
+    }];
+    
+    char lastC = '1';
+    NSMutableArray *data;
+    NSMutableArray *oth = [[NSMutableArray alloc] init];
+    for (HRContactModel *user in serializeArray) {
+        char c = [user.pinyin characterAtIndex:0];
+        if (!isalpha(c)) {
+            [oth addObject:user];
+        }
+        else if (c != lastC){
+            lastC = c;
+            if (data && data.count > 0) {
+                [ans addObject:data];
+            }
+            
+            data = [[NSMutableArray alloc] init];
+            [data addObject:user];
+        }
+        else {
+            [data addObject:user];
+        }
+    }
+    if (data && data.count > 0) {
+        [ans addObject:data];
+    }
+    if (oth.count > 0) {
+        [ans addObject:oth];
+    }
+    return ans;
+}
+
++ (NSMutableArray *)friendListSectionBy:(NSMutableArray *)array{
+    NSMutableArray *section = [[NSMutableArray alloc] init];
+    [section addObject:UITableViewIndexSearch];
+    for (NSArray *item in array) {
+        HRContactModel *user = [item objectAtIndex:0];
+        char c = [user.pinyin characterAtIndex:0];
+        if (!isalpha(c)) {
+            c = '#';
+        }
+        [section addObject:[NSString stringWithFormat:@"%c", toupper(c)]];
+    }
+    return section;
+}
+
+@end
